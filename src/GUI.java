1601c633ea6a58bb1e9bee5a5fe28a27f9bc9e16
MatRusTy <MatRusTy@users.noreply.github.com>
2018-12-20 04:11:31 +0100

    GUI progress

--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 
 public class GUI {
     private JFrame mainFrame;
+    private Container contentPane;
 
     public GUI(){
         createGUI();
@@ -10,11 +11,72 @@ public class GUI {
 
     private void createGUI(){
         mainFrame = new JFrame("Tetris");
-
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //createFile();
+        contentPane = mainFrame.getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        createContent();
 
         mainFrame.pack();
         mainFrame.setVisible(true);
     }
+
+    private void createContent(){
+
+        //GAME AREA
+        JPanel gameArea = setupGameArea();
+        gameArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        contentPane.add(gameArea, BorderLayout.CENTER);
+
+
+        //OPTIONS AREA
+        JPanel optionsArea = new JPanel();
+        optionsArea.setLayout(new BorderLayout());
+
+        // Label
+        JLabel optionsLabel = new JLabel("Options",SwingConstants.CENTER);
+        optionsArea.add(optionsLabel,BorderLayout.NORTH);
+
+        //Settings
+        JPanel settings = new JPanel();
+        settings.setLayout(new GridLayout(1,4));
+        optionsArea.add(settings);
+
+        JButton pauseResume = new JButton("Pause/Resume");
+        //ADD PAUSE FUNCTIONALITY HERE
+        settings.add(pauseResume);
+
+        JButton newGame = new JButton("New Game");
+        //ADD NEW GAME FUNCTIONALITY HERE
+        settings.add(newGame);
+
+        JButton settingsButton = new JButton("Settings");
+        //ADD SETTINGS FUNCTIONALITY HERE
+        settings.add(settingsButton);
+
+        JButton exitButton = new JButton("Exit Game");
+        exitButton.addActionListener(e -> System.exit(0)); //ADD POPUP ASKING IF THEY ARE SURE THEY WANT TO EXIT
+        settings.add(exitButton);
+
+        contentPane.add(optionsArea, BorderLayout.SOUTH);
+
+    }
+
+    private JPanel setupGameArea(){
+        //NEEDS IMPLEMENTING
+        return new JPanel();
+    }
+
+    private void createFile(){
+        JMenuBar menuBar = new JMenuBar();
+        mainFrame.setJMenuBar(menuBar);
+
+        JMenu test = new JMenu("Test");
+        JMenuItem testItem = new JMenuItem("Test");
+        test.add(testItem);
+        menuBar.add(test);
+    }
+
 
     public static void createGameGUI(){
         new GUI();
