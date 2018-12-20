c1aa21f13199a571a6efa5f0fa7c371e74fde664
MatRusTy <MatRusTy@users.noreply.github.com>
2018-12-20 02:10:04 +0100

    Added move right/left functionality to the tetriminos

--- src/Tetrimino.java
+++ src/Tetrimino.java
@@ -21,9 +21,34 @@ public abstract class Tetrimino {
 
     public abstract void calculateBottomPieces();
 
+    /**
+     * Moves the tetrimino one unit to the left
+     */
+    public void moveLeft(){
+        int indexOfRightermostBlock = Math.min(Math.min(pieces.get(0).x(),pieces.get(1).x()),Math.min(pieces.get(2).x(),pieces.get(3).x()));
+        if(indexOfRightermostBlock != 0){
+            for(IntPair i : pieces){
+                i.setX(i.x()-1);
+            }
+        }
+    }
+
+    /**
+     * Moves the tetrimino one unit to the right
+     */
+    public void moveRight(){
+        int indexOfRightermostBlock = Math.max(Math.max(pieces.get(0).x(),pieces.get(1).x()),Math.max(pieces.get(2).x(),pieces.get(3).x()));
+        if(indexOfRightermostBlock != 9){
+            for(IntPair i : pieces){
+                i.setX(i.x()+1);
+            }
+        }
+    }
+
+
     public void moveDown(){
         for(IntPair i : pieces){
-            i.setY(i.y()-1);
+            i.setY(i.y()+1);
         }
     }
 
